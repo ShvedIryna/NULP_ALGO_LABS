bfad1effbcf52b86a4550080f4cf9da597e98b6b
ShvedIryna <151570555+ShvedIryna@users.noreply.github.com>
2024-03-07 23:26:16 +0200

    Add files via upload

---
+++ unittest_for_lab2.py
@@ -0,0 +1,23 @@
+from lab2 import min_speed_of_eating_bananas
+import unittest
+
+
+class TestMyFunction(unittest.TestCase):
+    def test_min_speed_of_eating_bananas1(self):
+        piles = [3, 6, 7, 11]
+        h = 8
+        self.assertEqual(min_speed_of_eating_bananas(piles, h), 4)
+
+    def test_min_speed_of_eating_bananas2(self):
+        piles = [30, 11, 23, 4, 20]
+        h = 5
+        self.assertEqual(min_speed_of_eating_bananas(piles, h), 30)
+
+    def test_min_speed_of_eating_bananas3(self):
+        piles = [30, 11, 23, 4, 20]
+        h = 6
+        self.assertEqual(min_speed_of_eating_bananas(piles, h), 23)
+
+
+if __name__ == '__main__':
+    unittest.main()
