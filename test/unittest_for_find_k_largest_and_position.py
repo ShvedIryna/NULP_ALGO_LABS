7b555f4468a2c4c846686fac69fa31691effe74e
ShvedIryna <151570555+ShvedIryna@users.noreply.github.com>
2024-03-29 12:10:09 +0200

    Update and rename unittest_for_lab1.py to unittest_for_find_k_largest_and_position.py

--- test/unittest_for_find_k_largest_and_position.py
+++ test/unittest_for_find_k_largest_and_position.py
@@ -1,23 +1,23 @@
 import unittest
-from lab1 import compare_and_swap, find_k_largest, find_k_position
+from lab1 import quick_sort, find_k_largest, find_k_position
 
 
 class TestMyFunctions(unittest.TestCase):
 
     def test_swap(self):
         num = [15, 7, 22, 9, 36, 2, 42, 18]
-        sorted_num = compare_and_swap(num)
+        sorted_num = quick_sort(num)
         self.assertEqual(sorted_num, [2, 7, 9, 15, 18, 22, 36, 42])
 
     def test_find_k_largest(self):
         num = [15, 7, 22, 9, 36, 2, 42, 18]
-        sorted_num = compare_and_swap(num)
+        sorted_num = quick_sort(num)
         k_largest = find_k_largest(sorted_num, 3)
         self.assertEqual(k_largest, 22)
 
     def test_find_k_position(self):
         num = [15, 7, 22, 9, 36, 2, 42, 18]
-        sorted_num = compare_and_swap(num)
+        sorted_num = quick_sort(num)
         k_largest = find_k_largest(sorted_num, 3)
         position = find_k_position(sorted_num, 3)
         self.assertEqual(position, 5)
